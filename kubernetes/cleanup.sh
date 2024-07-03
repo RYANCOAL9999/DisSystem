1be1b26e96cb234df78cdc135e055d9e0c1e8f9f
RYANCOAL9999 <lmf242003@gmail.com>
2024-07-03 16:52:19 +0100

    add hpa and vpa

--- kubernetes/cleanup.sh
+++ kubernetes/cleanup.sh
@@ -3,4 +3,6 @@ kubectl delete services publisher rabbitMQ server
 kubectl delete deployments publisher rabbitMQ server
 kubectl delete secrets tls-certs
 kubectl delete configmaps rabbitmq-stable1-conf
-# kubectl delete scalings
+kubectl delete -f eks.yaml
+kubectl delete -f ./scaling/hpa_server.yaml -f ./scaling/hpa_publisher.yaml
+kubectl delete -f ./scaling/vpa_server.yaml -f ./scaling/vpa_publisher.yaml
